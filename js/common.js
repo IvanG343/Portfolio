954b0710e775d22f4c94b5005cb8f0fcda298490
IvanG343 <cobreg666@gmail.com>
2020-01-09 16:51:51 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul li a").click(function(act) {
+		act.preventDefault();
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if($(".top_mnu").is(":visible")) {
+			$(".top_text").removeClass("h_opacity");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").addClass("h_opacity");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	})
+
+});
+
+$(window).load(function() {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+});
